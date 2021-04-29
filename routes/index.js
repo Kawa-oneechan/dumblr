cd12dc22f75aab85bcd2249991a9503535281e0f
Kawa <kawaoneechan@gmail.com>
2021-04-29 11:54:18 +0200

    Switch to UUID instead of timestamps

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var mylogin = require('../login.js')
+const { v1: uuidv1 } = require('uuid');
 
 router.get('/', mylogin({allowGuest:false}), function(req, res, next) {
 	db.query("SELECT posts.*, users.handle, users.title AS `user-title` FROM posts LEFT JOIN follows ON posts.`user-id`=follows.target LEFT JOIN users ON users.id=posts.`user-id` WHERE follows.follower=? ORDER BY `posted-on` DESC", [req.user.id], function (error, results, fields) {
@@ -21,10 +22,13 @@ router.post('/', mylogin({allowGuest:true}), upload.any(), function(req, res, ne
 		res.sendStatus(403);
 	if (req.body['post-type'] == 'text')
 	{
+		console.log(req.body);
+
 		const stringTags = req.body['tags'].length > 2 ? JSON.stringify(req.body['tags'].split(', ').map(t => [t, sluggify(t)])) : '';
 		var timestamp = Date.time()
+		var uuid = uuidv1();
 		db.query("INSERT INTO posts SET ?", {
-			'id': timestamp,
+			'id': uuid,
 			'user-id': req.user.id,
 			'post-type': 'text',
 			'body-text': req.body['body-text'],
@@ -40,7 +44,7 @@ router.post('/', mylogin({allowGuest:true}), upload.any(), function(req, res, ne
 			'posted-on': timestamp
 		}, function(error, results, fields) {
 			if (error) throw error;
-			db.query("SELECT posts.*, users.handle, users.title AS `user-title` FROM posts LEFT JOIN follows ON posts.`user-id`=follows.target LEFT JOIN users ON users.id=posts.`user-id` WHERE posts.id = ?", [timestamp], function (error, results, fields) {
+			db.query("SELECT posts.*, users.handle, users.title AS `user-title` FROM posts LEFT JOIN follows ON posts.`user-id`=follows.target LEFT JOIN users ON users.id=posts.`user-id` WHERE posts.id = ?", [uuid], function (error, results, fields) {
 				if (error) throw error;
 				res.render('partials/post-loop', { posts: results, newPost: false, user: req.user, message: '' });
 			});
